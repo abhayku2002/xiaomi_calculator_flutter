f3fe1fc7a8c85fc6c9d834b44ba9870ceee89ea4
Abhay kumar <abhayku2002@gmail.com>
2021-11-24 15:06:12 +0530

    Update main.dart
    Updated and fix some bugs

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'Xiaomi Calculator Clone',
       home: XiaomiCalculator(),
@@ -55,7 +54,15 @@ class ResultsSpace extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(flex: 3, child: Align(alignment: Alignment.bottomRight,child: Text(' 0 ', textAlign: TextAlign.center,style: TextStyle(fontSize: 64),)));
+    return Expanded(
+        flex: 3,
+        child: Align(
+            alignment: Alignment.bottomRight,
+            child: Text(
+              ' 0 ',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 64),
+            )));
   }
 }
 
@@ -111,83 +118,120 @@ class CalculatorKeyGrid extends StatelessWidget {
       crossAxisSpacing: 20,
       crossAxisCount: 4,
       children: <Widget>[
+        CalculatorKey(Text(
+          'AC',
+          style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 30),
+        )),
+        CalculatorKey(Icon(
+          Icons.backspace,
+          color: Colors.deepOrangeAccent,
+        )),
         CalculatorKey(
-          Text('AC',
-          style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 30),)
+          Text(
+            '%',
+            style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 30),
+          ),
+        ),
+        CalculatorKey(Image.network(
+          'https://png.pngtree.com/svg/20161129/division_1251695.png',
+          color: Colors.deepOrangeAccent,
+          width: 21,
+          height: 21,
+        )),
+        CalculatorKey(
+          Text(
+            '7',
+            style: TextStyle(color: Colors.black, fontSize: 30),
+          ),
         ),
         CalculatorKey(
-          Icon(Icons.backspace, color: Colors.deepOrangeAccent,)
+          Text(
+            '8',
+            style: TextStyle(color: Colors.black, fontSize: 30),
+          ),
         ),
         CalculatorKey(
-          Text('%',
-          style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 30),),
+          Text(
+            '9',
+            style: TextStyle(color: Colors.black, fontSize: 30),
+          ),
         ),
         CalculatorKey(
-          Image.network('https://png.pngtree.com/svg/20161129/division_1251695.png',color: Colors.deepOrangeAccent,width: 21, height: 21,)
-        ),
-        
-        CalculatorKey(
-          Text('7',
-          style: TextStyle(color: Colors.black, fontSize: 30),),
+          Text(
+            'x',
+            style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 30),
+          ),
         ),
         CalculatorKey(
-          Text('8',
-          style: TextStyle(color: Colors.black, fontSize: 30),),
+          Text(
+            '4',
+            style: TextStyle(color: Colors.black, fontSize: 30),
+          ),
         ),
         CalculatorKey(
-          Text('9',
-          style: TextStyle(color: Colors.black, fontSize: 30),),
+          Text(
+            '5',
+            style: TextStyle(color: Colors.black, fontSize: 30),
+          ),
         ),
         CalculatorKey(
-          Text('x',
-          style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 30),),
+          Text(
+            '6',
+            style: TextStyle(color: Colors.black, fontSize: 30),
+          ),
         ),
         CalculatorKey(
-          Text('4',
-          style: TextStyle(color: Colors.black, fontSize: 30),),
+          Text(
+            '-',
+            style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 30),
+          ),
         ),
         CalculatorKey(
-          Text('5',
-          style: TextStyle(color: Colors.black, fontSize: 30),),
+          Text(
+            '1',
+            style: TextStyle(color: Colors.black, fontSize: 30),
+          ),
         ),
         CalculatorKey(
-          Text('6',
-          style: TextStyle(color: Colors.black, fontSize: 30),),
+          Text(
+            '2',
+            style: TextStyle(color: Colors.black, fontSize: 30),
+          ),
         ),
         CalculatorKey(
-          Text('-',
-          style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 30),),
+          Text(
+            '3',
+            style: TextStyle(color: Colors.black, fontSize: 30),
+          ),
         ),
         CalculatorKey(
-          Text('1',
-          style: TextStyle(color: Colors.black, fontSize: 30),),
+          Text(
+            '+',
+            style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 30),
+          ),
+        ),
+        CalculatorKey(Icon(
+          Icons.crop_rotate,
+          color: Colors.deepOrangeAccent,
+        )),
+        CalculatorKey(
+          Text(
+            '0',
+            style: TextStyle(color: Colors.black, fontSize: 30),
+          ),
         ),
         CalculatorKey(
-          Text('2',
-          style: TextStyle(color: Colors.black, fontSize: 30),),
+          Text(
+            '.',
+            style: TextStyle(color: Colors.black, fontSize: 30),
+          ),
         ),
         CalculatorKey(
-          Text('3',
-          style: TextStyle(color: Colors.black, fontSize: 30),),
-        ),
-        CalculatorKey(
-          Text('+',
-          style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 30),),
-        ),
-        CalculatorKey(
-          Icon(Icons.crop_rotate, color: Colors.deepOrangeAccent,)
-        ),
-        CalculatorKey(
-          Text('0',
-          style: TextStyle(color: Colors.black, fontSize: 30),),
-        ),
-        CalculatorKey(
-          Text('.',
-          style: TextStyle(color: Colors.black, fontSize: 30),),
-        ),
-        CalculatorKey(
-          Text('=',
-          style: TextStyle(color: Colors.white, fontSize: 30),),foreColor: Colors.deepOrangeAccent,
+          Text(
+            '=',
+            style: TextStyle(color: Colors.white, fontSize: 30),
+          ),
+          foreColor: Colors.deepOrangeAccent,
         ),
       ],
     );
@@ -198,11 +242,14 @@ class CalculatorKey extends StatelessWidget {
   final Widget widget;
   final Color color;
   final Color foreColor;
-  const CalculatorKey(this.widget,{
+  CalculatorKey(
+    this.widget, {
     this.color,
     this.foreColor,
     Key key,
+
   }) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
@@ -213,10 +260,8 @@ class CalculatorKey extends StatelessWidget {
       child: Container(
         margin: EdgeInsets.all(15),
         decoration: BoxDecoration(shape: BoxShape.circle, color: foreColor),
-        child: Center(
-          child: widget
-          ),
-        ),
+        child: Center(child: widget),
+      ),
     );
   }
 }
